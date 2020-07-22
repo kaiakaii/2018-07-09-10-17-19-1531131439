dffbfbad02670cfeaf0555af55f37621118e2d2e
sky <senkk512@163.com>
2020-07-22 21:15:05 +0800

    Practice10 Sky feature:add isTeaching method in Teacher Class and isIn method in Klss Class

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,68 @@
 package practice10;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+    private ArrayList<Student> classes;
+
+    public ArrayList<Student> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<Student> classes) {
+        this.classes = classes;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+        this.classes = new ArrayList<Student>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student) {
+        if (classes.contains(student)) {
+            this.leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        classes.add(student);
+    }
+    public boolean isIn(Student student){
+        if (classes.contains(student)){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,48 @@
 package practice10;
 
-public class Teacher {
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Teacher extends Person{
+    private Collection<Klass> classes;
+    public Teacher(int id,String name, int age, Collection<Klass> classes) {
+        super(id, name, age);
+        this.classes = new ArrayList<Klass>();
+    }
+
+    public Teacher(int id,String name, int age) {
+        super(id, name, age);
+    }
+
+    public Collection<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<Klass> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        String msg = "";
+        for (Klass klass:
+             classes) {
+            msg += klass.getNumber()+",";
+        }
+        return this.classes.size()!=0?super.introduce()+" I am a Teacher. I teach Class "+ msg.substring(0,msg.length()-1)+".":super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass:
+                this.classes) {
+            if (!klass.getClasses().contains(student)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
