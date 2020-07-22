f1fca75b146fc0df18ae985bc879f3575a470ab5
SUNSK <senkk512@163.com>
2020-07-22 17:35:10 +0800

    practice08 Sky feature:  save

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,42 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private String displayName;
+    private String leader;
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student.getName();
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,22 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private Klass kLass;
+
+    public Student(String name, int age, Klass kLass) {
+        super(name, age);
+        this.kLass = kLass;
+    }
+
+    public Klass getKlass() {
+        return kLass;
+    }
+
+    public void setkLass(Klass kLass) {
+        this.kLass = kLass;
+    }
+
+    public String introduce(){
+        return this.kLass.getLeader().equals("Tom")?super.introduce()+" I am a Student. I am Leader of Class "+this.kLass.getNumber()+".":super.introduce()+" I am a Student. I am at Class "+this.kLass.getNumber()+".";
+    }
 }
