075c6caaa45495a8c425fea5b196d44c49abbd43
SUNSK <senkk512@163.com>
2020-07-22 15:26:47 +0800

    practice02 Sky feature: Add kClass and introduce method in Student

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,15 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void introduce(String name, int age){
+        System.out.println("My name is "+name+". I am "+age+" years old.");
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,13 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    private int kClass;
+
+    public Student(String name,int age,int kClass) {
+        super(name,age);
+        this.kClass = kClass;
+    }
+    public void introduce(String name, int age){
+        System.out.println("I am a Student. I am at Class "+kClass);
+    }
 }
