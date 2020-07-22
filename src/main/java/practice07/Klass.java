edc9a8a1d0d94e87cb50a53055ea9c926cd5423c
SUNSK <senkk512@163.com>
2020-07-22 16:38:45 +0800

    practice07 Sky feature:  add number and displayName in Klass add introduceWith method in Teacher

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,29 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private String displayName;
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Klass() {
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass kLass;
+
+    public Student(String name, int age, Klass kLass) {
+        super(name, age);
+        this.kLass = kLass;
+    }
+
+    public Klass getKlass() {
+        return kLass;
+    }
+
+    public void setkLass(Klass kLass) {
+        this.kLass = kLass;
+    }
+
+    public String introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+this.kLass.getNumber()+".";
+    }
 }
