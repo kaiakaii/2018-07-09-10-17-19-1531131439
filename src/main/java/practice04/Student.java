5ab2a6b045e60ee464c46b38d92311c7e1028b22
SUNSK <senkk512@163.com>
2020-07-22 15:54:17 +0800

    practice04 Sky feature:  invoke Person's basicIntroduce retrun result

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,21 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int kLass;
+
+    public int getKlass() {
+        return kLass;
+    }
+
+    public void setKlass(int kClass) {
+        this.kLass = kClass;
+    }
+
+    public Student(String name, int age, int kClass) {
+        super(name,age);
+        this.kLass = kClass;
+    }
+    public String introduce(){
+        return "I am a Student. I am at Class "+this.kLass+".";
+    }
 }
